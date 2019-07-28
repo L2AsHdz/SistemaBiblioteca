08eb25bffcb7fe14513cad53e54510e1cda6c8cf
LAHdz <leonidas0197@gmail.com>
2019-07-28 16:10:03 -0600

    Interfaz preeliminar. Sin Funcionalidad :v

--- src/sistemabiblioteca/SistemaBiblioteca.java
+++ src/sistemabiblioteca/SistemaBiblioteca.java
@@ -1,9 +1,16 @@
 package sistemabiblioteca;
 
+import javax.swing.JFrame;
+import sistemabiblioteca.ui.Interfaz;
+
 public class SistemaBiblioteca {
 
     public static void main(String[] args) {
-        
+        Interfaz ui = new Interfaz();
+        ui.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        ui.setLocationRelativeTo(null);
+        ui.setTitle("Analizador Sintactico");
+        ui.setVisible(true);
     }
     
 }
