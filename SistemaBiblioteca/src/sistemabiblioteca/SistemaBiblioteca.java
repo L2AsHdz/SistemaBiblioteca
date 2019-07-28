d99b4197dc6b003cfdbf86e5baa32976175aefea
Kevin Escobar <kevin@mongefranco.com>
2019-07-28 01:42:15 -0600

    Ya lee archivo, y lo muestra en pantalla, aun no hace nada

--- SistemaBiblioteca/src/sistemabiblioteca/SistemaBiblioteca.java
+++ SistemaBiblioteca/src/sistemabiblioteca/SistemaBiblioteca.java
@@ -1,9 +1,13 @@
 package sistemabiblioteca;
 
+import sistemabiblioteca.interfaz.LecturaArchivoUI;
+
 public class SistemaBiblioteca {
 
     public static void main(String[] args) {
-        
+        LecturaArchivoUI login = new LecturaArchivoUI();
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
     }
     
 }
