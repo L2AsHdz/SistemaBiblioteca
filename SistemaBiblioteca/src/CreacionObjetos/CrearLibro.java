ef69fb8460347701ac64078869ea9000ac4b06ff
Kevin Escobar <kevin@mongefranco.com>
2019-07-28 21:26:06 -0600

    avance

--- SistemaBiblioteca/src/CreacionObjetos/CrearLibro.java
+++ SistemaBiblioteca/src/CreacionObjetos/CrearLibro.java
@@ -5,10 +5,39 @@
  */
 package CreacionObjetos;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import sistemabiblioteca.backend.Libro;
+
 /**
  *
  * @author daniel
  */
 public class CrearLibro {
+    //Solo acepta como parametro titulo, autor, codigo y cantidad
+    public static String crearLibro(String titulo, String autor, String codigo, int cantidad){
+        String mensaje;
+        
+        
+        
+        
+        Libro libro = new Libro(titulo, autor, codigo, cantidad);//Crea el objeto
+        persistirLibro(libro);//Crea el archivo binario con el libro
+        mensaje = "El libro " + titulo + " ha sido agregado exitosamente.";//Retorna un mensaje que se muestra en la interfaz grafica
+        return mensaje;
+    }
+    
+    public static void persistirLibro(Libro libro) {
+        File file = new File("Libro/" + libro.getCodigo() + ".bin");
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file);
+                ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);) {
+            outputStream.writeObject(libro);
+            outputStream.close();
+        } catch (IOException e) {
+            System.out.println("Error de conexion");
+        }
+    }
     
 }
---
+++ SistemaBiblioteca/src/CreacionObjetos/MetodosApoyo.java
@@ -0,0 +1,26 @@
+package CreacionObjetos;
+
+import java.io.File;
+
+/**
+ *
+ * @author daniel
+ */
+public class MetodosApoyo {
+    
+    //Verifica si existe un archivo en el nombre y la carpeta especificados, devuelve true o false
+    public static boolean verificarExistenciaArchivo(String nombre, String carpeta) {
+        boolean verificarExistenciaArchivo = false;
+        File folder = new File("./" + carpeta);
+        if (folder.isDirectory()) {
+            String[] files = folder.list();
+            for (String nombreArchivo : files) {
+                if (nombreArchivo.equals(nombre + ".bin")) {
+                        verificarExistenciaArchivo = true;
+                }
+            }
+        }
+        return verificarExistenciaArchivo;
+    }
+    
+}
